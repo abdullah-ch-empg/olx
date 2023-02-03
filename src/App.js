400dde0dc336970a37f82b3e1aa04935bc3dd37b
“abdullah-ch-empg” <abdullah.ch@empglabs.com>
2023-02-03 17:32:42 +0500

    feat: removed commented code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import "./App.css";
 import Login from "./Pages/Login";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  //  Navigate
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Protected from "./utils/ProtectedRoute";
-// import Cookies from "universal-cookie";
 import { Public } from "./Pages/Public";
 import { Error } from "./Pages/Error";
 
-// const cookies = new Cookies();
-
-// const isUserSignedIn = () => {
-//   let uid = cookies.get("uid");
-//   let accessToken = cookies.get("access-token");
-//   let client = cookies.get("client");
-
-//   return uid && accessToken && client ? true : false;
-// };
 function App() {
-  // const isSignedIn = isUserSignedIn();
   return (
     <div className="App">
       <BrowserRouter>
