7979d3be51b614df48f8a4a4fa5184a0b486c4e4
“abdullah-ch-empg” <abdullah.ch@empglabs.com>
2023-02-06 17:48:07 +0500

    feat: added persistance in designation's slice

--- src/App/store.js
+++ src/App/store.js
@@ -1,10 +1,39 @@
 import { configureStore } from "@reduxjs/toolkit";
 import clientSlice from "../Features/Client/clientSlice";
+import storage from "redux-persist/lib/storage";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import designationSlice from "../Features/Designation/designationSlice";
+// import thunk from "redux-thunk";
 
-export default configureStore({
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, designationSlice);
+
+export const store = configureStore({
   reducer: {
-    designation: designationSlice,
+    // designation: designationSlice,
     clients: clientSlice,
+    designation: persistedReducer,
+    // middleware: [thunk],
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
--- src/Pages/Public.jsx
+++ src/Pages/Public.jsx
@@ -1,5 +1,39 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectClients } from "../Features/Client/clientSlice";
+import { selectDesignation } from "../Features/Designation/designationSlice";
 
 export const Public = () => {
-  return <div>I am a Public Page, anybody can access me :-)</div>;
+  const designations = useSelector(selectDesignation);
+  const clients = useSelector(selectClients);
+
+  return (
+    <div>
+      I am a Public Page, anybody can access me :-)
+      {clients ? (
+        <ul>
+          {clients?.product_categories.map((designation) => (
+            <span key={designation.id}>
+              <li>{designation.name}</li>
+            </span>
+          ))}
+        </ul>
+      ) : (
+        <h1>NOT PERSISTED Client's Product Categories...........</h1>
+      )}
+      <h1>PERSISTED DESIGNATION DATA</h1>
+      {designations ? (
+        <ul>
+          {designations.map((designation) => (
+            <span key={designation.id}>
+              <li>{designation.name}</li>
+              {/* <button onClick={() => handleDelete(designation)}> Delete</button> */}
+            </span>
+          ))}
+        </ul>
+      ) : (
+        <h1>Loading...........</h1>
+      )}
+    </div>
+  );
 };
